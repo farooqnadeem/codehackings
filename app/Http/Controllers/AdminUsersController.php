7ec57bd65a00440051b6dcc71579daa6a2c62055
farooq nadeem <farooq.gcf@gmail.com>
2018-12-29 10:37:35 +0500

    commit all users changes before implement middleware admin security
    commit all users changes before implement middleware admin security

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserEditRequest;
 use App\Http\Requests\UsersRequest;
 use App\Photo;
 use App\Role;
@@ -94,9 +95,33 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(UsersRequest $request, $id)
+    public function update(UserEditRequest $request, $id)
     {
         //
+        $user= User::findOrFail($id);
+
+        if(trim($request->password)=='')
+        {
+            $input=$request->except('password');
+        }
+        else
+        {
+            $input= $request->all();
+            $input['password']=bcrypt($request->password);
+
+        }
+
+
+        if($file=$request->file('photo_id'))
+        {
+            $name = time().$file->getClientOriginalName();
+            $file->move('images',$name);
+            $photo = Photo::create(['file'=>$name]);
+            $input['photo_id']=$photo->id;
+        }
+
+        $user->update($input);
+        return redirect('/admin/users');
     }
 
     /**
